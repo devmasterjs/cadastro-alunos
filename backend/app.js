e821a31d4358bb9962c265eae89121d15ddae81c
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-06-19 12:39:39 -0300

    feat: added first route, controller and render minimal json

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,22 @@
-export default () => {
-  console.log('Hello World with Nodemon and Sucrase');
-};
+import express from 'express';
+import homeRoutes from './src/routes/homeRouter';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    // utilizado para manipular conteudos em requições POST
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
