5695f64b6f32ca696502ade72f1d00b1389fad46
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-06-26 11:31:36 -0300

    chore: dotenv config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import app from './app';
 
 const port = 3000;
 app.listen(port, () => {
+  console.log('------------------------------------------');
   console.log(`Backend-Server listen on port ${port}`);
   console.log(`CTRL + Click in http://localhost:${port}`);
+  console.log('------------------------------------------');
+  console.log(`Database Schema: ${process.env.DATABASE_SCHEMA}`);
+  console.log(`Database Host: ${process.env.DATABASE_HOST}`);
+  console.log(`Database Port: ${process.env.DATABASE_PORT}`);
+  console.log('------------------------------------------');
 });
