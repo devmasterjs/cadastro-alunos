66214b5f351f17fbce2eefcdb48ea1b7a3c93c95
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-07-17 11:59:41 -0300

    feat: created User Model

---
+++ backend/src/model/User.js
@@ -0,0 +1,43 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: {
+        type: Sequelize.STRING,
+        defaulValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo [name] deve possuir de 3 a 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        isEmail: {
+          len: {
+            msg: 'Email inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaulValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 12],
+            msg: 'A senha precisa possuir de 6 a 12 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+  }
+}
