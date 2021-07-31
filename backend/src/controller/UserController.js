f7d9f8624a9140abe871836345b24027ce67c748
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-07-31 12:34:09 -0300

    feat: created CRUD for users

--- backend/src/controller/UserController.js
+++ backend/src/controller/UserController.js
@@ -4,9 +4,75 @@ class UserController {
   async create(request, response) {
     try {
       const newUser = await User.create(request.body);
-      response.json(newUser);
+      return response.json(newUser);
     } catch (e) {
-      response.status(400).json({
+      return response.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async index(request, response) {
+    try {
+      const users = await User.findAll();
+      if (users) {
+        return response.json(users);
+      }
+      return response.json({ errors: 'Nenhum usuário cadastrado' });
+    } catch (e) {
+      return response.status(500).json({ errors: 'Ops! Ocorreu um problema. Tente novamente mais tarde!' });
+    }
+  }
+
+  async show(request, response) {
+    try {
+      const user = await User.findByPk(request.params.id);
+      if (user) {
+        return response.json(user);
+      }
+      return response.status(400).json({ errors: 'Usuário não encontrado' });
+    } catch (e) {
+      return response.status(500).json({ errors: 'Ops! Ocorreu um problema. Tente novamente mais tarde!' });
+    }
+  }
+
+  async update(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: 'ID não enviado' });
+      }
+
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({ errors: 'Usuário não encontrado' });
+      }
+
+      const updatedUser = await user.update(request.body);
+      return response.json(updatedUser);
+    } catch (e) {
+      return response.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(request, response) {
+    try {
+      if (!request.params.id) {
+        return response.status(400).json({ errors: 'ID não enviado' });
+      }
+
+      const user = await User.findByPk(request.params.id);
+
+      if (!user) {
+        return response.status(400).json({ errors: 'Usuário não encontrado' });
+      }
+
+      await user.destroy();
+      return response.json(user);
+    } catch (e) {
+      return response.status(400).json({
         errors: e.errors.map((error) => error.message),
       });
     }
