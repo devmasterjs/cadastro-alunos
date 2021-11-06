c28660ed73f64e5280dd8bec40cec209db74b81b
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-11-06 11:56:18 -0300

    feat: updated to refuse invalid image files

--- backend/src/config/multerConfig.js
+++ backend/src/config/multerConfig.js
@@ -2,6 +2,12 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  fileFilter: (req, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('O arquivo precisa ser do tipo PNG ou JPG.'));
+    }
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
