efc59bc6b857f50b66bb77c83bdfc9b8c43c6bc0
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2021-12-13 23:01:34 -0300

    feat: updated email field to be unique

---
+++ backend/src/database/migrations/20211214015241-change-email-aluno-unique.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('students', 'email',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      });
+  },
+
+  down: () => {},
+};
