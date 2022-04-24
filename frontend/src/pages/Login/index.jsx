b2ebb7173b673deb9d478b362fe75c10c3d654aa
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-04-24 11:36:04 -0300

    feat: react-toastify configuration

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,10 +1,23 @@
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
 export default function Login() {
+  const showMessage = () => {
+    toast.success('Login realizado com sucesso');
+    toast.error('Usuário ou senha inválida');
+    toast.warn('Senha informada muito fraca. Cuidado para não ser hackeado!');
+    toast.info(
+      'Não esqueça de pagar um pastel ao desenvolvedor no bar do Adão!'
+    );
+  };
+
   return (
     <Container>
       <Title isRed={false}>Login</Title>
+      <button type="button" onClick={showMessage}>
+        Login
+      </button>
     </Container>
   );
 }
