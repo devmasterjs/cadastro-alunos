eb43fbc9efc1e39b11ebc82e20489d2e24ad3602
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-08-28 11:09:04 -0300

    feat: created prevPath into location state

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import isEmail from 'validator/lib/isEmail';
+import { useLocation } from 'react-router-dom';
+import { get } from 'lodash';
 import { Container } from '../../styles/GlobalStyles';
 import { Form } from './styled';
 import * as actions from '../../store/modules/login/actions';
@@ -10,6 +12,8 @@ export default function Login() {
   const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const location = useLocation();
+  const prevPath = get(location, 'state.prevPath', '/');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,7 +31,7 @@ export default function Login() {
 
     if (formErrors) return;
 
-    dispatch(actions.doLoginRequest({ email, password }));
+    dispatch(actions.doLoginRequest({ email, password, prevPath }));
   };
 
   return (
