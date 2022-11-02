6ec036674099ccc7dc3792d948cd328bedef5b80
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-11-02 14:42:36 -0300

    feat: delete student

--- frontend/src/pages/Students/index.jsx
+++ frontend/src/pages/Students/index.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
 import { get } from 'lodash';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation,
+} from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
 import { ProfilePicture, StudentContainer } from './styled';
@@ -10,6 +16,7 @@ import Loading from '../../components/Loading';
 export default function Students() {
   const [students, setStudents] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getData = async () => {
@@ -22,33 +29,74 @@ export default function Students() {
     getData();
   }, []);
 
+  const handleDeleteAsk = (event) => {
+    event.preventDefault();
+    const deleteIcon = event.currentTarget;
+    const exclamationIcon = event.currentTarget.nextSibling;
+    exclamationIcon.setAttribute('display', 'block');
+    deleteIcon.remove();
+  };
+
+  const handleDelete = async (event, id, index) => {
+    try {
+      setLoading(true);
+      await axios.delete(`/students/${id}`);
+      const newStudents = [...students];
+      newStudents.splice(index, 1);
+      setStudents(newStudents);
+    } catch (error) {
+      const errors = get(error, 'response.data.errors', []);
+      const status = get(error, 'response.status', 0);
+      if (status === 401) {
+        toast.error('Você precisa fazer login');
+        navigate('/login');
+      } else {
+        errors.forEach((e) => toast.error(e));
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Listagem de alunos</h1>
 
       <StudentContainer>
-        {students.map((student) => (
-          <div key={student.id}>
+        {students.map((student, index) => (
+          <div id="row" key={String(student.id)}>
             <ProfilePicture>
               {get(student, 'Photos[0].url', false) ? (
-                <img src={student.Photos[0].url} alt="Foto do aluno" />
+                <img src={student.Photos[0].url} alt="" />
               ) : (
                 <FaUserCircle size={36} />
               )}
             </ProfilePicture>
-
-            <span className="info">
-              <span className="name">{student.name}</span>
+            <div id="student-id">
+              <div>{student.name}</div>
               <span>{student.email}</span>
-            </span>
+            </div>
 
-            <Link to={`/student/${student.id}/edit`}>
-              <FaEdit size={16} />
-            </Link>
-            <Link to={`/student/${student.id}/delete`}>
-              <FaWindowClose size={16} />
-            </Link>
+            <div id="action-buttons">
+              <Link to={`/student/${student.id}/edit`}>
+                <FaEdit size={16} />
+              </Link>
+
+              <Link
+                to={`/student/${student.id}/delete`}
+                onClick={handleDeleteAsk}
+              >
+                <FaWindowClose size={16} />
+              </Link>
+
+              <FaExclamation
+                size={16}
+                display="none"
+                cursor="pointer"
+                onClick={(e) => handleDelete(e, student.id, index)}
+              />
+            </div>
           </div>
         ))}
       </StudentContainer>
--- frontend/src/pages/Students/styled.js
+++ frontend/src/pages/Students/styled.js
@@ -1,41 +1,38 @@
 import styled from 'styled-components';
 
 export const StudentContainer = styled.div`
-  margin-top: 1.25rem;
-
-  div {
+  margin-top: 20px;
+  #row {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding: 0.3125rem 0;
+    padding: 5px 0;
+  }
 
-    .info {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      width: 80%;
-      padding-left: 10px;
-
-      span {
-        font-size: small;
-      }
-
-      .name {
-        font-size: medium;
-        font-weight: bold;
-      }
+  #student-id {
+    width: 70%;
+    span {
+      font-size: 12px;
     }
   }
 
-  div + div {
-    border-top: 0.0625rem solid #eee;
+  #row + #row {
+    border-top: 1px solid #eee;
+  }
+
+  #action-buttons {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 12%;
   }
 `;
 
 export const ProfilePicture = styled.div`
-  img {
-    width: 2.25rem;
-    height: 2.25rem;
+  img,
+  svg {
+    width: 36px;
+    height: 36px;
     border-radius: 50%;
   }
 `;
