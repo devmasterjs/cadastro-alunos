b1f07bd9fc75f9ffe8e4669f87b49b2180ed1eb0
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-11-13 12:08:13 -0300

    feat: search photo in the initial start photo page

--- frontend/src/pages/Photos/index.jsx
+++ frontend/src/pages/Photos/index.jsx
@@ -1,9 +1,51 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
 import { Container } from '../../styles/GlobalStyles';
+import * as actions from '../../store/modules/login/actions';
+import axios from '../../services/axios';
+import Loading from '../../components/Loading';
 
 export default function Photos() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState('');
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/students/${id}`);
+        const photoUrl = get(data, 'Photos[0].url', '');
+        setPhoto(photoUrl);
+        console.log(photo);
+      } catch (error) {
+        const errors = get(error, 'response.data.errors', []);
+        const status = get(error, 'response.status', 0);
+        if (status === 401) {
+          toast.error('Você precisa fazer login');
+          dispatch(actions.doLoginFailure());
+          navigate('/login');
+        } else {
+          errors.forEach((e) => toast.error(e));
+          navigate('/');
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <Container>
-      <h1>Galeria de fotos</h1>
+      <Loading isLoading={isLoading} />
+      <h1>Fotos</h1>
     </Container>
   );
 }
